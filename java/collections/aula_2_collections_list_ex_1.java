8636b4e2abec9aabacd8732b03ae61e9f78b0080
robsonoduarte <robson.o.d@gmail.com>
2026-09-16 20:46:32 -0300

    aula 2 collections

--- java/collections/aula_2_collections_list_ex_1.java
+++ java/collections/aula_2_collections_list_ex_1.java
@@ -1,31 +1,23 @@
 package collections;
 
-import org.w3c.dom.ls.LSOutput;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class aula_2_collections_list_ex_1{
 
-    // declar uma lista do tipo pessoa
-    // adicionar 3 pessoas utilizando o add
-    // fazer o print do nome das 3 pessoas utilizando o get
-    List<Pessoa> pessoas = new ArrayList<>();
+    public static void main(String[] args) {
+        // declar uma lista do tipo pessoa
+        // adicionar 3 pessoas utilizando o add
+        // fazer o print do nome das 3 pessoas utilizando o get
+        List<Pessoa> pessoas = new ArrayList<>();
 
-    Pessoa p1 = new Pessoa();
-    Pessoa p2 = new Pessoa();
-    Pessoa p3 = new Pessoa();
-    p1.
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+        Pessoa p3 = new Pessoa();
 
-
-
-
-
-
-
-
-
-
+        p1.nome = "Joao";
+    }
 
 }
