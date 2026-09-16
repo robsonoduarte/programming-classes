ed8ca001a9a6544198f9822967cae1597e18e358
ajsjsjdjdhhdhd <julio.farias1309@gmail.com>
2026-09-16 20:36:44 -0300

    aula 2 collection

--- java/collections/aula_2_collections_list_ex_1.java
+++ java/collections/aula_2_collections_list_ex_1.java
@@ -1,8 +1,31 @@
 package collections;
 
-public class aula_2_collections_list_ex_1 {
+import org.w3c.dom.ls.LSOutput;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class aula_2_collections_list_ex_1{
 
     // declar uma lista do tipo pessoa
     // adicionar 3 pessoas utilizando o add
     // fazer o print do nome das 3 pessoas utilizando o get
+    List<Pessoa> pessoas = new ArrayList<>();
+
+    Pessoa p1 = new Pessoa();
+    Pessoa p2 = new Pessoa();
+    Pessoa p3 = new Pessoa();
+    p1.
+
+
+
+
+
+
+
+
+
+
+
 }
